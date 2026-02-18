91b68b798f493a7070ce6ddb55b6a941c6ec4d90
Rai <geminiwalle7@gmail.com>
2026-02-18 14:24:07 +0100

    chore(web): finalize dev-origin config

--- clawos/apps/web/next.config.ts
+++ clawos/apps/web/next.config.ts
@@ -3,7 +3,7 @@ import { join } from "node:path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: join(__dirname, "../.."),
-  allowedDevOrigins: ["http://127.0.0.1:3100", "http://localhost:3100", "http://127.0.0.1:3000", "http://localhost:3000"]
+  allowedDevOrigins: ["127.0.0.1", "localhost"]
 };
 
 export default nextConfig;
